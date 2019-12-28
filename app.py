c2a3a303ccf809e8e8cd3ab49031b7c876be51cc
Maarten <maartenhop@outlook.com>
2019-12-28 13:44:43 +0100

    Tutorial finished

--- app.py
+++ app.py
@@ -84,6 +84,13 @@ def update_product(id):
     return product_schema.jsonify(product)
 
 
+#Delete product
+@app.route('/product/<id>', methods=['DELETE'])
+def delete_product(id):
+    product = Product.query.get(id)
+    db.session.delete(product)
+    db.session.commit()
+
 #run server
 if __name__ == '__main__':
     app.run(debug=True)
